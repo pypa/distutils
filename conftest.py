fc31db27ae312c899b990fa8aff32d28f1704c9f
Naveen M K <naveen521kk@gmail.com>
2021-12-15 20:08:49 +0530

    Add CI to test MSYS2-MinGW

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 import platform
-
+import sys
 
 collect_ignore = []
 
 
-if platform.system() != 'Windows':
+if "gcc" in sys.version.lower() or platform.system() != 'Windows':
     collect_ignore.extend([
         'distutils/command/bdist_msi.py',
         'distutils/msvc9compiler.py',
